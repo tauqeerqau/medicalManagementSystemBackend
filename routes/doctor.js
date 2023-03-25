9ed0bd9c353b758ff7347778bbfbbcbcb177924f
tauqeer <tauqeer.hassan@ideofuzion.com>
2023-03-25 12:43:32 +0500

    CRUD done for Doctor.

--- routes/doctor.js
+++ routes/doctor.js
@@ -23,6 +23,45 @@ con.connect(function(err) {
 });
 });
 
+router.get('/getDoctor', function(req, res, next) {
+    var id = req.query.id;
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Ideofuzion123!",
+      database:"medicalmanagementsystem"
+    });
+    
+    con.connect(function(err) {
+      if (err) res.send(err);
+      //res.send("Connected!");
+      var sql = `Select * from Doctor where ID=${id}`;
+      con.query(sql, function (err, result) {
+        if (err) res.send(err);
+        res.send(result);
+      });
+    });
+    });
+
+router.get('/deleteDoctor', function(req, res, next) {
+        var id = req.query.id;
+        var con = mysql.createConnection({
+          host: "localhost",
+          user: "root",
+          password: "Ideofuzion123!",
+          database:"medicalmanagementsystem"
+        });
+        
+        con.connect(function(err) {
+          if (err) res.send(err);
+          //res.send("Connected!");
+          var sql = `Delete from Doctor where ID=${id}`;
+          con.query(sql, function (err, result) {
+            if (err) res.send(err);
+            res.send(result);
+          });
+        });
+    });
 
 
 router.post('/addDoctor', function(req, res, next) {
@@ -52,5 +91,42 @@ router.post('/addDoctor', function(req, res, next) {
     });
   });
   });
+
+
+  router.post('/updateDoctor', function(req, res, next) {
+
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Ideofuzion123!",
+      database:"medicalmanagementsystem"
+    });
+    
+    con.connect(function(err) {
+      if (err) res.send(err);
+      var id = req.body.id;
+      var name = req.body.name;
+      var phoneNumber = req.body.phoneNumber;
+      var age = req.body.age;
+      var cnic = req.body.cnic;
+      var email = req.body.email;
+      var address = req.body.address;
+      var gender = req.body.gender;
+      
+      var sql = `Update Doctor set Name='${name}',
+      PhoneNumber='${phoneNumber}',
+      Age=${age},
+      CNIC='${cnic}',
+      Email='${email}',
+      Address='${address}',
+      Gender='${gender}'
+      where ID = ${id}`;
+      
+      con.query(sql, function (err, result) {
+        if (err) res.send(err);
+        res.send(result);
+      });
+    });
+    });
 
 module.exports = router;
